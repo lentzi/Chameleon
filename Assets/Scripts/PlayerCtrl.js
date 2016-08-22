4411fff6f1d9483ebf01e3d8b3926052b1246d8b
Andrei Radulescu <aradulescu@smallfootprint.com>
2016-08-22 11:26:39 +0300

    Created play area, add movement to ball and attached camera to it

---
+++ Assets/Scripts/PlayerCtrl.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+private var rb : Rigidbody;
+var speed : float;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+}
+
+function FixedUpdate () {
+
+	var moveHorizontal : float = Input.GetAxis("Horizontal");
+	var moveVertical : float = Input.GetAxis("Vertical");
+
+	var movement : Vector3 = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+	rb.AddForce (movement * speed);
+}
